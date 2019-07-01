a1eb77a10aed7085138d8e4f031a5b1d4d55cc84
K-Bermet <bkalbotoeva@gmail.com>
2019-06-30 23:23:42 -0500

    created Udemy app

---
+++ Udemy/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  Udemy
+//
+//  Created by tilekbek kadyrov on 6/30/19.
+//  Copyright © 2019 tilekbek kadyrov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        
+        VStack(alignment: .center) {
+            Image("image")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+//                .clipShape(Circle())
+                .cornerRadius(8)
+                .padding()
+            
+            
+            Text("First line")
+                .font(.largeTitle)
+                .foregroundColor(.green)
+            Text("Second line")
+                .font(.title)
+                .foregroundColor(.orange)
+            HStack {
+                Text("Left Side")
+                Text("Right Side")
+                    .padding(.all)
+            }
+        }
+        
+    }
+}
+
