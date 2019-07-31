cdb075974c00df3ec9bf966e179398d37efe7599
K-Bermet <bkalbotoeva@gmail.com>
2019-07-31 00:14:03 -0500

    API building implementing Webservice

---
+++ Udemy/NewsAPI/Service/WebService.swift
@@ -0,0 +1,28 @@
+//
+//  WebService.swift
+//  Udemy
+//
+//  Created by tilekbek kadyrov on 7/31/19.
+//  Copyright © 2019 tilekbek kadyrov. All rights reserved.
+//
+
+import Foundation
+
+class Webservice {
+    func loadTopHeadlines(url: URL, completion: @escaping ([Article]?) -> ()) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            guard let data = data, error == nil else {
+                completion(nil)
+                return
+            }
+            
+            let response = try? JSONDecoder().decode(NewsResponse.self, from: data)
+            if let response = response {
+                DispatchQueue.main.async {
+                    completion(response.articles)
+                }
+            }
+        }.resume()
+    }
+}
---
+++ Udemy/NewsAPI/View/NewsView.swift
@@ -0,0 +1,17 @@
+//
+//  NewsView.swift
+//  Udemy
+//
+//  Created by tilekbek kadyrov on 7/28/19.
+//  Copyright © 2019 tilekbek kadyrov. All rights reserved.
+//
+
+import SwiftUI
+
+struct NewsView : View {
+        
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
